378f0e379e03bc371fb415d40623560ddc52a21a
vcannings <veritycannings@gmail.com>
2015-04-28 20:51:54 +0100

    added basic jQuery functions

--- Parallax/scripts/scripts.js
+++ Parallax/scripts/scripts.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
     up a css class for this and set the display: none; property. This might 
     avoid seeing the content briefly before our script file loads and hides it. 
     */
-    $("#theImage").hide();     
+    
+    $("#navImage").hide();
     
     /*
     Function that gets called every time the page is scrolled in either direction. 
@@ -18,13 +19,24 @@ $(document).ready(function() {
         console.log(scrollPosition); 
        
         if(scrollPosition > 350) {
-            $("#theImage").fadeIn();
+            $("#navImage").fadeIn();
         }
         
         if(scrollPosition < 350) {
-            $("#theImage").fadeOut();
+            $("#navImage").fadeOut();
+        }
+        
+        if(scrollPosition > 350) {
+            $("#headerImage").fadeOut(800);
+        }
+        
+        if(scrollPosition < 350) {
+            $("#headerImage").fadeIn(400);
         }
         
     });
     
+    $( "#navImage" ).click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
 });
